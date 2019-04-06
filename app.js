0db423a27f1de91cb09acfe5a35594827868a840
Odumosu Oluwashina <odumz20@gmail.com>
2019-04-06 12:51:12 +0100

    Add files via upload

---
+++ app.js
@@ -0,0 +1,11 @@
+(function() {
+	"use strict";
+	
+	var LandingView = React.createElement(
+	"div",
+	{ className: "landingView" },
+	"This is just me"
+	);
+	
+	ReactDom.render(LandingView, document.getElementById('react-root'));
+})();
